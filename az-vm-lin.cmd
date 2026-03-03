5c3780c5f14194b2c84f6c084956cf0d19078ff6
Hasan Ozdemir <hasanozdemirnet@gmail.com>
2026-03-03 06:06:02 +0300

    chore: add elevated root launchers for linux and windows flows

---
+++ az-vm-lin.cmd
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo - You are not running with administrator privileges.
+    echo - Restarting with elevated privileges...
+    start powershell -Command "Start-Process -FilePath '%~f0' -ArgumentList '%*' -Verb RunAs"
+    if %errorlevel% neq 0 (
+        echo !!! Error: Failed to obtain administrator permission. Please run cmd with 'Run as administrator'.!!!
+        pause
+    )
+    exit /b
+)
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0lin-vm\az-vm-lin.ps1" %*
+endlocal
