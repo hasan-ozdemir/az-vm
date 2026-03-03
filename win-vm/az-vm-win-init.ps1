a681a7792210d3bce8e149ea0237e60e81924ddb
Hasan Ozdemir <hasanozdemirnet@gmail.com>
2026-03-03 18:29:40 +0300

    chore: ignore auto-generated vm init/update artifacts

--- win-vm/az-vm-win-init.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ErrorActionPreference = "Stop"
-$ProgressPreference = "SilentlyContinue"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Write-Output "Init phase started."
-Set-TimeZone -Id "UTC" -ErrorAction SilentlyContinue
-Write-Output "Init phase completed."
